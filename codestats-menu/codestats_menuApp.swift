d405bb02fb0e9493cecd167930d4a2d46794dc2f
Arshak Aghakaryan <agakaryan.arshak@gmail.com>
2023-12-20 18:08:17 +0400

    Initial Commit

---
+++ codestats-menu/codestats_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  codestats_menuApp.swift
+//  codestats-menu
+//
+//  Created by arshak ‎ on 20.12.23.
+//
+
+import SwiftUI
+
+@main
+struct codestats_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
